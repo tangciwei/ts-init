f7479801c7cd9ef11424d16ed533a02c9b6c04ab
唐词伟 <tangciwei@bytedance.com>
2019-05-14 16:46:37 +0800

    ok

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const R = require("ramda");
-R.path;
+const l = console.log;
+var articles = [
+    {
+        title: "Everything Sucks",
+        url: "http://do.wn/sucks.html",
+        author: {
+            name: "Debbie Downer",
+            email: "debbie@do.wn"
+        }
+    },
+    {
+        title: "If You Please",
+        url: "http://www.geocities.com/milq",
+        author: {
+            name: "Caspar Milquetoast",
+            email: "hello@me.com"
+        }
+    }
+];
+// var isAuthor = R.identity; // change this
+// assertEqual(false, isAuthor("New Guy", articles));
+// assertEqual(true, isAuthor("Debbie Downer", articles));
+function isAuthor(name, arr) {
+    return !!R.find(item => item === name)(arr.map(R.path(['author', 'name'])));
+}
+l(1111, isAuthor("Debbie Downer", articles));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-import * as R from 'ramda'
-R.path
+import * as R from "ramda";
+const l = console.log;
+
+var articles = [
+  {
+    title: "Everything Sucks",
+    url: "http://do.wn/sucks.html",
+    author: {
+      name: "Debbie Downer",
+      email: "debbie@do.wn"
+    }
+  },
+  {
+    title: "If You Please",
+    url: "http://www.geocities.com/milq",
+    author: {
+      name: "Caspar Milquetoast",
+      email: "hello@me.com"
+    }
+  }
+];
+// var isAuthor = R.identity; // change this
+// assertEqual(false, isAuthor("New Guy", articles));
+// assertEqual(true, isAuthor("Debbie Downer", articles));
+function isAuthor(name,arr){
+   return !!R.find(item=>item===name)(arr.map(R.path(['author','name']))) 
+}
+
+l(1111,isAuthor("Debbie Downer", articles));
